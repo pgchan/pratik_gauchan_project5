15258d107d4264d82ddd1edc1ab9cf4f60bed0b2
Pratik <pgauchan@gmail.com>
2018-09-21 00:58:38 -0400

    searchbar updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ class Header extends Component {
             <header className="header">
                 {/* <h1 className="header__header">News</h1> */}
                 <a href="index.html">
-                    <img className="header__logo" src="../../googlenews.png" alt="Main logo (google news) that hides in mobile view." />
+                    <img className="header__logo" src="./googlenews.png" alt="Main logo (google news) that hides in mobile view." />
                 </a>
                 <a href="index.html">
-                    <img className="header__logo__icon" src="../../googlenewsIcon.png" alt="Google News logo that is displayed in mobile view." />
+                    <img className="header__logo__icon" src="./googleNewsIcon.png" alt="Google News logo that is displayed in mobile view." />
                 </a>
             </header>
         );
